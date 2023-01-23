1017326343679a947cf87f55525b596a3bd4bbd9
bocampagni <a.bocampagni@gmail.com>
2023-01-23 20:33:22 -0300

    feat: udp listening on generic port

--- cmd/main/hereWe.go
+++ cmd/main/hereWe.go
@@ -3,12 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
-
+	"net"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 func main() {
+
+	p, err := net.ListenPacket("udp", ":1053")
+	if err != nil {
+		panic(err)
+	}
+	defer p.Close()
+
+
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
@@ -26,4 +34,18 @@ func main() {
 		fmt.Println(container.Names)
 		fmt.Println(container.NetworkSettings.Networks["bridge"].IPAddress)
 	}
+
+	for {
+		buf := make([]byte, 512)
+		n, addr, err := p.ReadFrom(buf)
+		if err != nil {
+			fmt.Printf("Connection error [%s]: %s\n", addr.String(), err)
+			continue
+		}
+
+		fmt.Println(n)
+		fmt.Println(addr)
+	}
+
+
 }
